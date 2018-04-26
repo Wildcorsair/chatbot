018ba13bdee42995b3662ff344e0b40a93839b4b
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2018-04-26 22:50:52 +0300

    Initial commit

---
+++ chatbot.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3001);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Hello world!!!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Server was started on the port: ' + app.get('port'));
+});
